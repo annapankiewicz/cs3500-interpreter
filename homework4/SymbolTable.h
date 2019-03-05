448197f00dc8c93542484b10587f7214ad049c23
Anna Pankiewicz <anna.pankiewicz13@gmail.com>
2019-03-04 21:42:16 -0600

    Add ability to change an existing ident's type

--- homework4/SymbolTable.h
+++ homework4/SymbolTable.h
@@ -29,9 +29,23 @@ public:
     else return(false);
   }
 
+  bool changeEntry(SYMBOL_TABLE_ENTRY x)
+  {
+    // Make sure the entry we want to change in the symbol table is
+    // actually in the symbol table.
+    map<string, SYMBOL_TABLE_ENTRY>::iterator itr;
+    if ((itr = hashTable.find(x.getName())) != hashTable.end())
+    {
+      hashTable.erase(itr);
+      hashTable.insert(make_pair(x.getName(), x));
+      return true;
+    }
+    else return(false);
+  }
+
   // If a SYMBOL_TABLE_ENTRY with name theName is
-  // found in this symbol table, then return true;
-  // otherwise, return false.
+  // found in this symbol table, then return its type info;
+  // otherwise, return undefined.
   TYPE_INFO findEntry(string theName)
   {
     TYPE_INFO info = {UNDEFINED, NOT_APPLICABLE, NOT_APPLICABLE};
